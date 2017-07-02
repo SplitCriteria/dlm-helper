6e6235b75898d23d419d9f69621ec4047c167edd
Nick Johnson <bs01@splitcriteria.com>
2017-07-01 14:48:28 -1000

    Removed unused function from common.php

--- common.php
+++ common.php
@@ -1,6 +1,5 @@
 <?php
 namespace SplitCriteria\DLMHelper;
-
 /**
 	Appends the right string to the left string on a new line.
 	
@@ -15,27 +14,4 @@ function appendOnNewLine(&$left, $right) {
 		$left = (is_null($left) ? "" : $left) . (empty($left) ? "" :  "\n") .  $right;
 	}
 }
-
-/**
-	Determines if a date is valid by using several methods.
-
-	@param date	a date string
-	@return bool
-*/
-function isDateValid($date) {
-	/* Set a default timezone to suppress warnings */
-	date_default_timezone_set("UTC");
-	/* Assume the date is invalid */
-	$isValid = false;
-	/* Check with php function strtotime and DateTime::createFromFormat -- since
-	   strtotime doesn't understand all date formats. */
-	$result = strtotime($date);
-	if (($result && $result != -1)
-		|| \DateTime::createFromFormat("m-d-Y", $date)
-		|| \DateTime::createFromFormat("m/d/Y", $date)) {
-		$isValid = true;
-	}
-	return $isValid;
-}
-
 ?>
